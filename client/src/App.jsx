798f1c285ef7a1749aa980555151f97dc3c808f5
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-05-21 16:29:10 +0530

    starting with frontend

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,13 @@
-
-
 function App() {
   
+  /* add layouts here */
+
+
   return (
-    <>
-      <div className="text-xl">Home</div>
-    </>
-  )
+      <div className="min-h-screen grid place-items-center">
+        <h1 className="text-3xl text-emerald-500">Project running!</h1>
+      </div>
+  );
 }
 
 export default App
