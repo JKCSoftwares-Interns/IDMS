5484e4c1fc858e954b06371b17a348a0aa530503
Om Dixit <omnathdixit7033@gmail.com>
2024-05-24 00:13:33 +0530

    Design completed of Navbar

--- client/src/Layouts/Router.jsx
+++ client/src/Layouts/Router.jsx
@@ -1,22 +1,23 @@
 import React from 'react';
-import {  Route, Routes, NavLink } from 'react-router-dom';
+import {  Route, Routes } from 'react-router-dom';
 import Homepage from '../Pages/Homepage';
 import Inventory from '../Pages/inventory/viewProducts';
 import Report from '../Pages/Report';
 import Help from '../Pages/Help';
 import Settings from '../Pages/Settings';
 import ContactUs from '../Pages/ContactUs';
+import AddProducts from '../Pages/inventory/addProducts';
 const Routing = () => {
     return (
         
-        
         <Routes>
             <Route path="/" element={<Homepage />} />
-            <Route path="/Inventory" element={<Inventory />} />
-            <Route path="/Report" element={<Report />} />
-            <Route path="/Help" element={<Help />} />
-            <Route path="/Settings" element={<Settings />} />
-            <Route path="/ContactUs" element={<ContactUs />} />
+            <Route path="/inventory" element={<Inventory />} />
+            <Route path="/report" element={<Report />} />
+            <Route path="/help" element={<Help />} />
+            <Route path="/settings" element={<Settings />} />
+            <Route path="/contactus" element={<ContactUs />} />
+            <Route path="/addproducts" element={<AddProducts />} />
         </Routes>
     
     );
