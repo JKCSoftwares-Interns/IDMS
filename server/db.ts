7c723132d84df39917bc273f7f742baa3063449d
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-05-27 06:26:49 +0530

    backend functionalities completed

--- server/db.ts
+++ server/db.ts
@@ -1,23 +1,24 @@
-import mariadb, { PoolConnection } from 'mariadb';
+import mariadb, { PoolConnection } from "mariadb";
 
-const pool = mariadb.createPool({
-     host: 'localhost', 
-     user:'root', 
-     password: 'test',
-     port: 3306,
-     connectionLimit: 5
+export const pool = mariadb.createPool({
+	host: "localhost",
+	user: "root",
+	password: "test",
+	port: 3306,
+	connectionLimit: 5,
 });
 
-async function createTables() {
+export async function initializeDB() {
+	console.log("Initializing database...");
 
-  console.log("creating table");
-
-  let conn: PoolConnection | null = null;
-  try {
-    conn = await pool.getConnection();
-    await conn.query(`
-    CREATE TABLE Products (
-      productId VARCHAR(255) PRIMARY KEY,
+	let conn: PoolConnection | null = null;
+	try {
+		conn = await pool.getConnection();
+		await conn.query(`CREATE DATABASE IF NOT EXISTS testidms;`);
+		await conn.query(`USE testidms;`);
+		await conn.query(`
+    CREATE TABLE IF NOT EXISTS products (
+      productId INT AUTO_INCREMENT PRIMARY KEY,
       productName VARCHAR(255),
       category VARCHAR(255),
       measuringUnit VARCHAR(255),
@@ -36,46 +37,16 @@ async function createTables() {
       cess DECIMAL(10,2),
       loadPrice DECIMAL(10,2),
       unloadingPrice DECIMAL(10,2),
-      dateAdded TIMESTAMP,
+      dateAdded TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
       addedBy VARCHAR(255),
       lastEditedDate TIMESTAMP,
       lastEditedBy VARCHAR(255)
     );
-      `);
-  } catch (err) {
-    console.log(err);
-  } finally {
-    if (conn) conn.release();
-  }
+`);
+		console.log("Database initialized successfully.");
+	} catch (err) {
+		console.error("Error initializing database: ", err);
+	} finally {
+		if (conn) conn.release();
+	}
 }
-
-/* will be used in orm */
-interface Product {
-  productId: string;
-  productName: string;
-  category: string;
-  measuringUnit: string;
-  packSize: number;
-  noOfUnits: number;
-  unitMRP: number;
-  packMRP: number;
-  manufacturer: string;
-  marketer: string;
-  supplier: string;
-  upc: string;
-  hsn: string;
-  cgst: number;
-  sgst: number;
-  igst: number;
-  cess: number;
-  loadPrice: number;
-  unloadingPrice: number;
-  dateAdded: Date;
-  addedBy: string;
-  lastEditedDate: Date;
-  lastEditedBy: string;
-}
-
-createTables();
-
-export default pool
