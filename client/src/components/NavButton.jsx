90d5b64aa27351d9aa8aa3f3d19556b0611def77
Kinjalk Tripathi <kinjalkt3@gmail.com>
2024-05-30 13:19:49 +0530

    minor checkpoint

--- client/src/components/NavButton.jsx
+++ client/src/components/NavButton.jsx
@@ -2,13 +2,19 @@ import { NavLink } from "react-router-dom";
 import { Button } from "@mui/material";
 
 const NavButton = ({ to, icon, label, color }) => {
-  return (
-    <NavLink className="mx-6" to={to}>
-      <Button startIcon={icon} variant="text" color={color} sx={{ textTransform: 'none', fontSize: 16 }}>
-        {label}
-      </Button>
-    </NavLink>
-  );
+	return (
+		<NavLink className="mx-6" to={to}>
+			<Button
+				startIcon={icon}
+				variant="text"
+        activeClassName="active-link"
+				color={color}
+				sx={{ textTransform: "none", fontSize: 16 }}
+			>
+				{label}
+			</Button>
+		</NavLink>
+	);
 };
 
 export default NavButton;
--- client/src/layouts/Sidebar.jsx
+++ client/src/layouts/Sidebar.jsx
@@ -9,68 +9,73 @@ import AssessmentRoundedIcon from "@mui/icons-material/AssessmentRounded";
 import StorefrontRoundedIcon from '@mui/icons-material/StorefrontRounded';
 
 const Sidebar = () => {
-	const BUTTON_COLOR = "primary";
+    const BUTTON_COLOR = "primary";
 
-	return (
-		<motion.div
-			initial={{
-				translateX: "100px",
-			}}
-			animate={{
-				translateX: "0px",
-			}}
-			transition={{
-				duration: 1,
-				type: "spring",
-			}}
-			className="flex flex-col border rounded-xl px-6 mb-5 py-4 gap-6 backdrop-filter backdrop-blur-lg bg-white bg-opacity-90"
-		>
-			<div className="flex flex-col gap-4 mt-3">
-				<NavButton
-					to="/"
-					icon={<SpaceDashboardRoundedIcon />}
-					label="Dashboard"
-					color={BUTTON_COLOR}
-				/>
-				<NavButton
-					to="/inventory"
-					icon={<Inventory2RoundedIcon />}
-					label="Inventory"
-					color={BUTTON_COLOR}
-				/>
-				<NavButton
-					to="/products"
-					icon={<CategoryRoundedIcon />}
-					label="Products"
-					color={BUTTON_COLOR}
-				/>
-				<NavButton
-					to="/vendors"
-					icon={<StorefrontRoundedIcon />}
-					label="Vendors"
-					color={BUTTON_COLOR}
-				/>
-				<NavButton
-					to="/transport"
-					icon={<LocalShippingRoundedIcon />}
-					label="Transport"
-					color={BUTTON_COLOR}
-				/>
-				<NavButton
-					to="/offers"
-					icon={<LocalOfferRoundedIcon />}
-					label="Offers"
-					color={BUTTON_COLOR}
-				/>
-				<NavButton
-					to="/reports"
-					icon={<AssessmentRoundedIcon />}
-					label="Reports"
-					color={BUTTON_COLOR}
-				/>
-			</div>
-		</motion.div>
-	);
+    const navButtons = [
+        {
+            to: "/",
+            icon: <SpaceDashboardRoundedIcon />,
+            label: "Dashboard",
+        },
+        {
+            to: "/inventory",
+            icon: <Inventory2RoundedIcon />,
+            label: "Inventory",
+        },
+        {
+            to: "/products",
+            icon: <CategoryRoundedIcon />,
+            label: "Products",
+        },
+        {
+            to: "/vendors",
+            icon: <StorefrontRoundedIcon />,
+            label: "Vendors",
+        },
+        {
+            to: "/transport",
+            icon: <LocalShippingRoundedIcon />,
+            label: "Transport",
+        },
+        {
+            to: "/offers",
+            icon: <LocalOfferRoundedIcon />,
+            label: "Offers",
+        },
+        {
+            to: "/reports",
+            icon: <AssessmentRoundedIcon />,
+            label: "Reports",
+        },
+    ];
+
+    return (
+        <motion.div
+            initial={{
+                translateX: "100px",
+            }}
+            animate={{
+                translateX: "0px",
+            }}
+            transition={{
+                duration: 1,
+                type: "spring",
+            }}
+            className="flex flex-col border rounded-xl px-6 mb-5 py-4 gap-6 backdrop-filter backdrop-blur-lg bg-white bg-opacity-90"
+        >
+            <div className="flex flex-col gap-4 mt-3">
+                {navButtons.map((button, index) => (
+                    <NavButton
+                        key={index}
+                        to={button.to}
+                        icon={button.icon}
+                        label={button.label}
+                        color={BUTTON_COLOR}
+                    />
+                ))}
+            </div>
+        </motion.div>
+    );
 };
 
 export default Sidebar;
